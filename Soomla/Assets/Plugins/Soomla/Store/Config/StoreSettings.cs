7dafa022db2623790b22def73f025320a876a38a
Dima Nemets <dima@soom.la>
2014-11-18 14:35:35 +0200

    compilation directory structure changed - divided into platforms (android, ios)

--- Soomla/Assets/Plugins/Soomla/Store/Config/StoreSettings.cs
+++ Soomla/Assets/Plugins/Soomla/Store/Config/StoreSettings.cs
@@ -181,7 +181,7 @@ namespace Soomla.Store
 		}
 
 		private Dictionary<string, bool> bpUpdate = new Dictionary<string, bool>();
-		private static string bpRootPath = Application.dataPath + "/Soomla/compilations/android-billing-services/";
+		private static string bpRootPath = Application.dataPath + "/Soomla/compilations/android/android-billing-services/";
 
 		public static void handlePlayBPJars(bool remove) {
 			try {
