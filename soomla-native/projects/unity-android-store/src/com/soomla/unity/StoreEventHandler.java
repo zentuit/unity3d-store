25140587ea3c94d75035ec50460d95413716cb03
Eliot <fadookie@gmail.com>
2015-02-27 20:59:22 +0200

    In StoreEventHandler.onMarketPurchase, pass back originalJson and signature to Unity for server-side receipt verification

--- soomla-native/projects/unity-android-store/src/com/soomla/unity/StoreEventHandler.java
+++ soomla-native/projects/unity-android-store/src/com/soomla/unity/StoreEventHandler.java
@@ -224,6 +224,8 @@ public class StoreEventHandler {
             JSONObject extraJSON = new JSONObject();
             extraJSON.put("purchaseToken", playPurchaseEvent.getToken());
             extraJSON.put("orderId", playPurchaseEvent.getOrderId());
+            extraJSON.put("originalJson", playPurchaseEvent.getOriginalJson());
+            extraJSON.put("signature", playPurchaseEvent.getSignature());
             eventJSON.put("extra", extraJSON);
 
             UnityPlayer.UnitySendMessage("StoreEvents", "onMarketPurchase", eventJSON.toString());
