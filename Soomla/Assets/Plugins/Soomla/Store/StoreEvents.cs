d7b44e45ff4ac6fdbb217124c93a3fd83522ff06
Tom Jenkins <tjenkins@afterinsanity.com>
2015-07-11 09:50:53 -0400

    Removed #if DEBUG_SOOMLA copy/paste error Added more detailed comment on when the event happens

--- Soomla/Assets/Plugins/Soomla/Store/StoreEvents.cs
+++ Soomla/Assets/Plugins/Soomla/Store/StoreEvents.cs
@@ -651,15 +651,16 @@ namespace Soomla.Store {
         
         /// <summary>
         /// Handles the <c>onMarketPurchaseDeferred</c> event, which is fired when a Market purchase was deferred
-        /// until it can be finished by the family delegate
+        /// until it can be finished by the family delegate.
+        /// Note that this is an iOS only event for when users have set up "Ask to Buy" and the purchaser is 
+        /// selected as a family member that needs "family organizer" permission to buy.
+        /// <see href="https://support.apple.com/en-us/HT201089">Apple's explanation of "Ask to Buy"</see> 
         /// </summary>
         /// <param name="message">Message that contains information about the market purchase that is being
         /// deferred.</param>
         public void onMarketPurchaseDeferred(string message) {
-            #if DEBUG_SOOMLA
             SoomlaUtils.LogDebug(TAG, "SOOMLA/UNITY onMarketPurchaseDeferred: " + message);
-            #endif
-            
+
             var eventJSON = new JSONObject(message);
             
             PurchasableVirtualItem pvi = (PurchasableVirtualItem)StoreInfo.GetItemByItemId(eventJSON["itemId"].str);
